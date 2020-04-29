ac849804457d68e230385a3b19aee4dddc652ed4
boss <ggrahl18@gmail.com>
2020-04-29 00:28:13 -0500

    node best practises of structure

--- models/blog.js
+++ models/blog.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose')
+
 mongoose.set('useFindAndModify', false)
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB', error.message)
-  })
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(result => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch((error) => {
+//     console.log('error connecting to MongoDB', error.message)
+//   })
 
 const blogSchema = new mongoose.Schema({
   title: {
