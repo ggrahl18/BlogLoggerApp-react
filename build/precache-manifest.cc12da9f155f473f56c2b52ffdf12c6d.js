b6fca8a0993c4cd15cc6f1aea2ae203ad6d3a126
boss <ggrahl18@gmail.com>
2020-05-09 21:22:19 -0500

    togglable added, notifications work

--- build/precache-manifest.cc12da9f155f473f56c2b52ffdf12c6d.js
+++ build/precache-manifest.cc12da9f155f473f56c2b52ffdf12c6d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbfb8c411f4b682b9f08fd1fe143e285",
+    "revision": "f1357ca2cd498d505600d0568c133b70",
     "url": "/index.html"
   },
   {
-    "revision": "37cd9fa335f55dafb1b4",
+    "revision": "336df9fc51b89077c763",
     "url": "/static/css/main.533de4b1.chunk.css"
   },
   {
-    "revision": "f02f9987560defb010ed",
-    "url": "/static/js/2.7daf743f.chunk.js"
+    "revision": "1e1e2230ed3d0d76c890",
+    "url": "/static/js/2.24dec358.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.7daf743f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.24dec358.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "37cd9fa335f55dafb1b4",
-    "url": "/static/js/main.704329d6.chunk.js"
+    "revision": "336df9fc51b89077c763",
+    "url": "/static/js/main.ff319794.chunk.js"
   },
   {
     "revision": "25f2d7a006535c6287b1",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82d6157536861876856bebf87a981648.js"
+  "/precache-manifest.cc12da9f155f473f56c2b52ffdf12c6d.js"
 );
 
 self.addEventListener('message', (event) => {
